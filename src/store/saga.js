c29c1263ee70786f99cefd893c2b2a992e4e0135
Colin <516114821@qq.com>
2018-02-28 16:53:25 +0800

    再加一个现在比较时髦的写法，本质是一样的

--- src/store/saga.js
+++ src/store/saga.js
@@ -14,30 +14,45 @@ function* fetchData(action) {
 function* fetchData1() {
   try {
     const data = yield getOnePerson();
-    yield put({type: types.FETCHING_PERSON_SUCCESS, data1:data});
+    yield put({type: types.FETCHING_PERSON_SUCCESS, data1: data});
   } catch (e) {
     yield put({type: types.FETCHING_PERSON_FAILURE});
   }
 }
 
+// export function* watchfetchData() {
+//   while (true) {
+//     yield take(types.FETCHING_DATA);
+//     yield fork(fetchData);
+//   }
+// }
+//
+// export function* watchfetchData1() {
+//   while (true) {
+//     yield take(types.FETCHING_DATA1);
+//     yield fork(fetchData1);
+//   }
+// }
+//
+// function* dataSaga() {
+//   yield [fork(watchfetchData), fork(watchfetchData1)];
+// }
+
+//另外一种写法，takeEvery是高阶写法，其实内部就是上面的写法
 export function* watchfetchData() {
-  while (true) {
-    yield take(types.FETCHING_DATA);
-    yield fork(fetchData);
-  }
+  yield takeEvery(types.FETCHING_DATA, fetchData);
 }
 
 export function* watchfetchData1() {
-  while (true) {
-    yield take(types.FETCHING_DATA1);
-    yield fork(fetchData1);
-  }
+  yield takeEvery(types.FETCHING_DATA1, fetchData1);
 }
 
+//加不加all，表达的是一个意思，切记不能将数组分开，不然会产生阻塞现象，
+// 第二个action在第一个没有完成前不会触发
+// 另外fork是无阻塞的意思
 function* dataSaga() {
-  yield [fork(watchfetchData), fork(watchfetchData1)];
+  yield all([fork(watchfetchData), fork(watchfetchData1)]);
 }
 
-// fork(takeEvery(types.FETCHING_DATA1, fetchData1)),
 
 export default dataSaga;
